b787d49f6232dff11b35a75147701d5e2de00b19
pstryuk <pavel.stryuk.ps@gmail.com>
2024-01-22 00:18:21 +0300

    KS-1-3 did

---
+++ src/main/kotlin/lesson_2/l1_t3.kt
@@ -0,0 +1,13 @@
+package lesson_2
+
+fun main() {
+    val flight_year: Int = 1961
+    var flight_hour = 9
+    var flight_minute = 7
+    println(flight_year)
+    println(flight_hour)
+    println(flight_minute)
+    flight_hour = 10
+    flight_minute = 55
+    println("[" + flight_hour + ":" + flight_minute + "]")
+}
