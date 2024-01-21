e862f3e8d3bb5fbce78aff149b32e4ea6f288957
pstryuk <pavel.stryuk.ps@gmail.com>
2024-01-22 00:08:35 +0300

    KS-1-2 did

---
+++ src/main/kotlin/lesson_1/l1_t2.kt
@@ -0,0 +1,10 @@
+package lesson_1
+
+fun main() {
+    var sales_quantity: Int = 42
+    var text_order: String = "Спасибо за заказ!"
+    println("Количество продаж: " + sales_quantity + "\n" + text_order)
+//    var workers_count = 2000
+    var workers_count = 1999
+    println("Количество работников: " + workers_count)
+}
