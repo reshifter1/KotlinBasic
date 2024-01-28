23329057b7e3acc9fae9f9202fbd42d53bf2dd56
pstryuk <pavel.stryuk.ps@gmail.com>
2024-01-28 22:24:10 +0300

    KS-3-2 did

---
+++ src/main/kotlin/lesson_3/l3-t2.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+fun main() {
+    val age20: Byte = 20
+    val fio20: String = "Андреева Татьяна Сергеевна"
+    val age22: Byte = 22
+    val fio22: String = "Сидорова Татьяна Сергеевна"
+
+    println("[$fio20, $age20]")
+    println("[$fio22, $age22]")
+}
