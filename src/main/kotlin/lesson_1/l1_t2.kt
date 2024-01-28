f80d7bdc61b31dc1bad79658b6d902251443e3e7
pstryuk <pavel.stryuk.ps@gmail.com>
2024-01-28 18:39:51 +0300

    KS-1-2 fixed error

--- src/main/kotlin/lesson_1/l1_t2.kt
+++ src/main/kotlin/lesson_1/l1_t2.kt
@@ -1,10 +1,13 @@
 package lesson_1
 
 fun main() {
-    var sales_quantity: Int = 42
-    var text_order: String = "Спасибо за заказ!"
-    println("Количество продаж: " + sales_quantity + "\n" + text_order)
+    var salesQuantity: Int = 42
+    var textOrder: String = "Спасибо за заказ!"
+
+    println("Количество продаж: " + salesQuantity + "\n" + textOrder)
+
 //    var workers_count = 2000
-    var workers_count = 1999
-    println("Количество работников: " + workers_count)
+    var workersCount = 1999
+
+    println("Количество работников: " + workersCount)
 }
