54206d67a02ba2daaaec686d8b229b8443464690
pstryuk <pavel.stryuk.ps@gmail.com>
2024-01-29 23:15:46 +0300

    KS-4-1 fix

--- src/main/kotlin/lesson_4/l4-t1.kt
+++ src/main/kotlin/lesson_4/l4-t1.kt
@@ -1,9 +1,11 @@
 package lesson_4
 
+const val totalTables: Byte = 13
+
 fun main() {
     val tomorrowTablesLeft: Byte = 9
     val todayTablesLeft: Byte = 13
-    val totalTables: Byte = 13
+
     val todayNote: String = "Доступность столиков на сегодня:"
     val tomorrowNote: String = "Доступность столиков на завтра:"
 
