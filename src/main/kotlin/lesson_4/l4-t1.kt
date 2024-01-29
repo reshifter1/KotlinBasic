4cb068c0d2551f09cd642283e55fb466690a71cb
pstryuk <pavel.stryuk.ps@gmail.com>
2024-01-29 22:57:37 +0300

    KS-4-1 did

--- src/main/kotlin/lesson_4/l4-t1.kt
+++ src/main/kotlin/lesson_4/l4-t1.kt
@@ -1,5 +1,11 @@
 package lesson_4
 
 fun main() {
+    val tomorrowTablesLeft: Byte = 9
+    val todayTablesLeft: Byte = 13
+    val totalTables: Byte = 13
+    val todayNote: String = "Доступность столиков на сегодня:"
+    val tomorrowNote: String = "Доступность столиков на завтра:"
 
+    println("[$todayNote ${totalTables - todayTablesLeft > 0}],\n[$tomorrowNote ${totalTables - tomorrowTablesLeft > 0}].")
 }
