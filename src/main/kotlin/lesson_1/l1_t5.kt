5d063e47c007530177bb143c42acb69a288b89c9
pstryuk <pavel.stryuk.ps@gmail.com>
2024-01-29 23:00:05 +0300

    KS-1-5 fix again

--- src/main/kotlin/lesson_1/l1_t5.kt
+++ src/main/kotlin/lesson_1/l1_t5.kt
@@ -1,9 +1,10 @@
 package lesson_1
 
+const val SECS_IN_MINUTE: Int = 60
+const val MINS_IN_HOUR: Int = 60
+
 fun main() {
     var seconds: Int = 6480
-    val SECS_IN_MINUTE: Int = 60
-    val MINS_IN_HOUR: Int = 60
 
     val minutes: Int = seconds / MINS_IN_HOUR % SECS_IN_MINUTE
     val hours: Int = seconds / (MINS_IN_HOUR * SECS_IN_MINUTE)
