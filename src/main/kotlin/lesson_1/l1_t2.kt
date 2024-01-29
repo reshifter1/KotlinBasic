fdd175bf8debe39a3fab6f429c1b608ad18af60c
pstryuk <pavel.stryuk.ps@gmail.com>
2024-01-29 22:14:50 +0300

    KS-1-2 fix

--- src/main/kotlin/lesson_1/l1_t2.kt
+++ src/main/kotlin/lesson_1/l1_t2.kt
@@ -1,7 +1,7 @@
 package lesson_1
 
 fun main() {
-    var salesQuantity: Int = 42
+    var salesQuantity: Int = 75
     var textOrder: String = "Спасибо за заказ!"
 
     println("Количество продаж: " + salesQuantity + "\n" + textOrder)
