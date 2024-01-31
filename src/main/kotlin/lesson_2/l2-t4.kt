3e92b71ac96e0b8bb67bf2e2c088f50a06754301
pstryuk <pavel.stryuk.ps@gmail.com>
2024-01-31 23:52:33 +0300

    KS-2-4 fix

--- src/main/kotlin/lesson_2/l2-t4.kt
+++ src/main/kotlin/lesson_2/l2-t4.kt
@@ -3,11 +3,13 @@ package lesson_2
 fun main() {
     val crystals: Int = 7
     val iron: Int = 11
-    val buff: Float = 0.2F
+    val buff: Int = 20
+    val buff_percent: Float = buff.toFloat() / 100
 
-    val crystalsBuffed: Int = (crystals * buff).toInt()
-    val ironBuffed: Int = (iron * buff).toInt()
+    val crystalsBuffed: Int = (crystals * buff_percent).toInt()
+    val ironBuffed: Int = (iron * buff_percent).toInt()
 
+    println(buff_percent)
     println(crystalsBuffed)
     println(ironBuffed)
 }
