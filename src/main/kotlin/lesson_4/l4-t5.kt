3dc4cd9b1e917bd69e3bf3a3de3e577ea163a3d2
pstryuk <pavel.stryuk.ps@gmail.com>
2024-02-05 22:42:11 +0300

    KS-4-5 formatting

--- src/main/kotlin/lesson_4/l4-t5.kt
+++ src/main/kotlin/lesson_4/l4-t5.kt
@@ -12,19 +12,19 @@ const val ALT_FOOD_NUMBER = 50
 const val ALT_WEATHER_CONDITION = true
 
 fun main() {
-    val shipDamaged: Boolean = readln().toBoolean()
+    val isShipDamaged: Boolean = readln().toBoolean()
     val crewNumber: Int = readln().toInt()
     val foodNumber: Int = readln().toInt()
-    val goodWeather: Boolean = readln().toBoolean()
+    val isGoodWeather: Boolean = readln().toBoolean()
 
     println(
         (
-                (shipDamaged || (SAFE_SHIP_CONDITION)) && crewNumber in MIN_CREW_NUMBER..MAX_CREW_NUMBER &&
-                        (foodNumber > MIN_FOOD_NUMBER) && (goodWeather || (!WEATHER_CONDITION))
+                (isShipDamaged || (SAFE_SHIP_CONDITION)) && crewNumber in MIN_CREW_NUMBER..MAX_CREW_NUMBER &&
+                        (foodNumber > MIN_FOOD_NUMBER) && (isGoodWeather || (!WEATHER_CONDITION))
                 )
                 || (
-                (shipDamaged || (ALT_SAFE_SHIP_CONDITION)) && crewNumber == ALT_CREW_NUMBER &&
-                        (foodNumber >= ALT_FOOD_NUMBER) && (goodWeather || (!ALT_WEATHER_CONDITION))
+                (isShipDamaged || (ALT_SAFE_SHIP_CONDITION)) && crewNumber == ALT_CREW_NUMBER &&
+                        (foodNumber >= ALT_FOOD_NUMBER) && (isGoodWeather || (!ALT_WEATHER_CONDITION))
                 )
     )
 }
