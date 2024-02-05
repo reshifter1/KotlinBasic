5bd17c500972e0fe9feccb989b0e1d9982bd9ba0
pstryuk <pavel.stryuk.ps@gmail.com>
2024-02-05 22:37:10 +0300

    KS-4-3 fix

--- src/main/kotlin/lesson_4/l4-t3.kt
+++ src/main/kotlin/lesson_4/l4-t3.kt
@@ -4,10 +4,11 @@ const val WET = 20
 const val BAD_SEASON = "зима"
 
 fun main() {
-    val weather = true
-    val tent = true
+    val isSunnyWeather = true
+    val isOpenTent = true
     val wet = 20
     val season = "зима"
 
-    println(weather && tent && (wet == WET) && (season != BAD_SEASON))
+    println("Благоприятные ли условия сейчас для роста бобовых? " +
+        (isSunnyWeather && isOpenTent && (wet == WET) && (season != BAD_SEASON)))
 }
