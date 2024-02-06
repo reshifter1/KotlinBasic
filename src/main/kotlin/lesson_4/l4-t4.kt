cdd1b8eda8a754ca9545a7bd81391bb9d5cc3abe
pstryuk <pavel.stryuk.ps@gmail.com>
2024-02-06 18:15:35 +0300

    KS-4-4 fix

--- src/main/kotlin/lesson_4/l4-t4.kt
+++ src/main/kotlin/lesson_4/l4-t4.kt
@@ -1,19 +1,13 @@
 package lesson_4
 
-const val isEven = 0
-const val isOdd = 1
-
-const val HANDS = isOdd
-const val LEGS = isEven
-const val BACK = isEven
-const val PRESS = isOdd
 fun main() {
     val dayNumber = 5
+    val isEven = (dayNumber % 2 == 0)
 
     println("""
-        Упражнения для рук:    ${dayNumber % 2 == HANDS}
-        Упражнения для ног:    ${dayNumber % 2 == LEGS}
-        Упражнения для спины:  ${dayNumber % 2 == BACK}
-        Упражнения для пресса: ${dayNumber % 2 == PRESS}
+        Упражнения для рук:    ${!isEven}
+        Упражнения для ног:    ${isEven}
+        Упражнения для спины:  ${isEven}
+        Упражнения для пресса: ${!isEven}
     """.trimIndent())
 }
