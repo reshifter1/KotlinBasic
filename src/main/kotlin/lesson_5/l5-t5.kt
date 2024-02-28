6567c85bf410b0e3f15c1937e3ea56491567243d
pstryuk <pavel.stryuk.ps@gmail.com>
2024-02-28 12:10:23 +0300

    KS-5-5 fix

--- src/main/kotlin/lesson_5/l5-t5.kt
+++ src/main/kotlin/lesson_5/l5-t5.kt
@@ -1,18 +1,15 @@
 package lesson_5
 
-import kotlin.random.Random
-
 const val LIMIT_OF_RANDOM = 42
-const val START_FROM_1 = 1
 
 const val JACKPOT = 3
 const val BIG_WIN = 2
 const val SMALL_WIN = 1
 
 fun main() {
-    val randomNumber1: Int = START_FROM_1 + Random.nextInt(LIMIT_OF_RANDOM)
-    val randomNumber2: Int = START_FROM_1 + Random.nextInt(LIMIT_OF_RANDOM)
-    val randomNumber3: Int = START_FROM_1 + Random.nextInt(LIMIT_OF_RANDOM)
+    val randomNumber1: Int = (1..LIMIT_OF_RANDOM).random()
+    val randomNumber2: Int = (1..LIMIT_OF_RANDOM).random()
+    val randomNumber3: Int = (1..LIMIT_OF_RANDOM).random()
 
     val randomNumbers: Array<Int> = arrayOf(randomNumber1, randomNumber2, randomNumber3)
 
