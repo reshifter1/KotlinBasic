87727ae49c83808a6e9c4f91a6704b358052162a
pstryuk <pavel.stryuk.ps@gmail.com>
2024-02-28 23:02:13 +0300

    KS-7-4 did

---
+++ src/main/kotlin/lesson_7/l7-t4.kt
@@ -0,0 +1,12 @@
+package lesson_7
+
+fun main() {
+    println("Введите количество секунд:")
+
+    for (i in readln().toInt() downTo 1) {
+        println("Осталось $i секунд(ы)")
+        Thread.sleep(/* millis */ 1000)
+    }
+
+    println("Время вышло!")
+}
