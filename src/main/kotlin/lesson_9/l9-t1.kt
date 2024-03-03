3bfcb6416f7f82c5ddb4de038793ef5f72639c71
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-03 23:53:35 +0300

    KS-9-1 did

--- src/main/kotlin/lesson_9/l9-t1.kt
+++ src/main/kotlin/lesson_9/l9-t1.kt
@@ -1,2 +1,8 @@
 package lesson_9
 
+fun main() {
+    val recipe = listOf("рис", "куриное филе", "соевый соус", "щепотка любви")
+
+    println("В рецепте есть следующие ингридиенты:")
+    recipe.forEach { println(it) }
+}
