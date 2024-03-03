5b96dedc1fcbe9df1716ca8bd20820a49d3fb2bf
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-04 00:09:58 +0300

    KS-9-3 formatting

--- src/main/kotlin/lesson_9/l9-t2.kt
+++ src/main/kotlin/lesson_9/l9-t2.kt
@@ -3,12 +3,12 @@ package lesson_9
 fun main() {
     val recipe = mutableListOf("рис", "соевый соус", "щепотка любви")
 
-    println("В списке есть базовые ингридиенты: ${recipe.joinToString()}")
+    println("В списке есть базовые ингридиенты: ${recipe}")
     println("Желаете добавить ещё? Напишите \"да\", если желаете.")
 
     if (readln() !in arrayOf("да", "Да")) return
 
     println("Какой ингридиент вы хотите добавить?")
     recipe.add(readln())
-    println("Теперь в рецепте следующие ингридиенты: ${recipe.joinToString()}")
+    println("Теперь в рецепте следующие ингридиенты: ${recipe}")
 }
