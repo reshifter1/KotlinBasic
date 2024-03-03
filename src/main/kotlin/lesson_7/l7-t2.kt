e5629e56b80b275341314991287aa4b66ba4c405
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-03 22:24:18 +0300

    KS-7-2 formatting

--- src/main/kotlin/lesson_7/l7-t2.kt
+++ src/main/kotlin/lesson_7/l7-t2.kt
@@ -1,11 +1,13 @@
 package lesson_7
 
 fun main() {
-    while (true) {
-        val code = (1000..9999).random().toString()
+    var code = "0000"
+    var input = ""
+    while (code != input) {
+        code = (1000..9999).random().toString()
         println("Ваш код авторизации: $code")
         println("Введите код:")
-        if (readln() == code) break
+        input = readln()
     }
     println("Привет!!!")
 }
