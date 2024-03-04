4014f5777a820e4db2fa998d8c947136408c0890
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-05 00:47:52 +0300

    KS-11-2 did

---
+++ src/main/kotlin/lesson_11/User2.kt
@@ -0,0 +1,24 @@
+package lesson_11
+
+class User2 (
+    val id: Int,
+    val username: String,
+    var password: String,
+    val email: String,
+    var bio: String = ""
+){
+    fun printParametres() {println("$id:$username:$password:$email")}
+    fun setPassword() {
+        println("Введите текущий пароль:")
+        if (readln() == password) {
+            println("Введите новый пароль:")
+            password = readln()
+            println("Пароль обновлён")
+        } else println("Неверный пароль")
+    }
+    fun setBio() {
+        println("Введите биографию:")
+        bio = readln()
+        println("Биография обновлена")
+    }
+}
---
+++ src/main/kotlin/lesson_11/l11-t2.kt
@@ -0,0 +1,9 @@
+package lesson_11
+
+fun main() {
+    val user2 = User2(2, "pavel", "123", "fvjriv@mail.ru")
+
+    user2.setBio()
+    user2.setPassword()
+    user2.printParametres()
+}
