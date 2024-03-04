fba65b1ed7378d7ff2ae578e80de285924493b4f
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-04 17:09:56 +0300

    KS-10-2 did

---
+++ src/main/kotlin/lesson_10/l10-t2.kt
@@ -0,0 +1,16 @@
+package lesson_10
+
+fun validateLength(data: String): Boolean {
+    if (data.count() < 4) return false
+    else return true
+}
+
+fun main() {
+    println("Введите логин:")
+    val username = readln()
+    println("Введите пароль:")
+    val password = readln()
+
+    if (validateLength(username) && validateLength(password)) println("Привет!")
+        else println("Логин или пароль недостаточно длинные")
+}
