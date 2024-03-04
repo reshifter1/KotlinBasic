10da84cfe1e9a2f49b57522e5a94b6a29e25b1eb
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-04 16:58:46 +0300

    KS-10-1 did

--- src/main/kotlin/lesson_10/l10-t1.kt
+++ src/main/kotlin/lesson_10/l10-t1.kt
@@ -1,2 +1,16 @@
 package lesson_10
 
+val throwDice: () -> Int = {(1..6).random()}
+fun main() {
+    val personThrow = throwDice()
+    println("Человек выбросил $personThrow")
+
+    val robotThrow = throwDice()
+    println("Компьютер выбросил $robotThrow")
+
+    when {
+        personThrow > robotThrow -> println("Победило человечество")
+        personThrow < robotThrow -> println("Победила машина")
+        else -> println("Ничья!")
+    }
+}
