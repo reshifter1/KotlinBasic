c2c220e2062263cb159d4692185adbf6e43fcbbe
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-05 18:15:55 +0300

    KS-12-1 did

---
+++ src/main/kotlin/lesson_12/Weather.kt
@@ -0,0 +1,7 @@
+package lesson_12
+
+class Weather (_dayWeather: Int, _nightWeather: Int, _hasFallout: Boolean) {
+    var dayWeather = _dayWeather
+    var nightWeather = _nightWeather
+    var hasFallout = _hasFallout
+}
--- src/main/kotlin/lesson_12/l12-t1.kt
+++ src/main/kotlin/lesson_12/l12-t1.kt
@@ -1,2 +1,9 @@
-package lesson_10
+package lesson_12
+
+fun main() {
+    val day1 = Weather(32, 12, true)
+    val day2 = Weather(31, 22, false)
+    day2.hasFallout = true
+    println(day2.hasFallout)
+}
 
