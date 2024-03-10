ba8699342c57efcfe72560caeac8362b86d1d938
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-10 17:57:13 +0300

    KS-9-5 did

--- src/main/kotlin/lesson_9/l9-t5.kt
+++ src/main/kotlin/lesson_9/l9-t5.kt
@@ -8,7 +8,5 @@ fun main() {
         recipeSet.add(readln())
     }
 
-    val recipe: MutableList<String> = recipeSet.sorted().toMutableList()
-    recipe[0] = recipe[0].replaceFirstChar {it.uppercase()}
-    println(recipe.joinToString(", "))
+    println(recipeSet.sorted().joinToString(", ").replaceFirstChar { it.uppercase() })
 }
