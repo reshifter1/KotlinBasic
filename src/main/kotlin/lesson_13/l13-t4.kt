d1159b902fe86e686cb89d0e87d511a914331ae6
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-11 22:59:24 +0300

    KS-13-4 did

---
+++ src/main/kotlin/lesson_13/l13-t4.kt
@@ -0,0 +1,31 @@
+package lesson_13
+
+fun main() {
+    val contacts = mutableListOf<Person4>()
+
+    var oneMorePhone = "да"
+    while (oneMorePhone.equals("да", ignoreCase = true)) {
+        println("Введите номер телефона:")
+        val number = readln().toLongOrNull()
+
+        if (number == null) {
+            println("Номер пуст или присутствуют символы!")
+            continue
+        }
+
+        println("Введите имя:")
+        val name = readln()
+
+        println("Введите компанию:")
+        var company: String? = readln()
+        if (company == "") company = null
+
+        contacts.add(Person4(name = name, number = number, company = company))
+
+        println("Добавить ещё? контакт в телефонную книгу?")
+        oneMorePhone = readln()
+    }
+
+    for (person in contacts) person.print()
+
+}
