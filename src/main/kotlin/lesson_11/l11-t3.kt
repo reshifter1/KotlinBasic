eb0928976740ced21ea9473aea2b45dcf710e730
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-12 17:41:20 +0300

    little refactor

--- src/main/kotlin/lesson_11/l11-t3.kt
+++ src/main/kotlin/lesson_11/l11-t3.kt
@@ -12,3 +12,37 @@ fun main() {
     room.newStatus("Pavel", "пользователь заглушен")
     room.getUserInfo("Pavel")
 }
+
+
+class Room (
+    var icon: String,
+    var name: String,
+    val userMap: MutableMap<String, User3>,
+) {
+    fun addUser(user3: User3) {
+        userMap[user3.nickname] = user3
+    }
+
+    fun newStatus(username: String, status: String) {
+        try {
+            userMap[username]!!.status = status
+        } catch (e: Exception) { println("Ошибка: ${e.javaClass.simpleName}") }
+    }
+
+    fun getUserInfo(username: String) {
+        try {
+            userMap[username]!!.getInfo()
+        } catch (e: Exception) { println("Ошибка: ${e.javaClass.simpleName}") }
+    }
+}
+
+
+class User3(
+    val nickname: String,
+    var avatar: String,
+    var status: String,
+) {
+    fun getInfo() {
+        println("$nickname:$avatar:$status")
+    }
+}
