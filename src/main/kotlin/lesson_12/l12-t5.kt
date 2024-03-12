8660d15e6e7fe8783c382987ea3f41774b707341
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-12 17:51:44 +0300

    little refactor

--- src/main/kotlin/lesson_12/l12-t5.kt
+++ src/main/kotlin/lesson_12/l12-t5.kt
@@ -2,7 +2,7 @@ package lesson_12
 
 const val KELVIN_DIFFERENCE: Float = 273.15F
 
-class Weather5 (_dayWeather: Float, _nightWeather: Float, _hasFallout: Boolean) {
+class Weather5(_dayWeather: Float, _nightWeather: Float, _hasFallout: Boolean) {
     var dayWeather = _dayWeather - KELVIN_DIFFERENCE
     var nightWeather = _nightWeather - KELVIN_DIFFERENCE
     var hasFallout = _hasFallout
