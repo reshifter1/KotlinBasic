eb3c7f6ea3c4f51f56c9216fcbd64186055ff952
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-12 17:53:00 +0300

    little refactor

--- src/main/kotlin/lesson_13/Person.kt
+++
@@ -1,7 +0,0 @@
-package lesson_13
-
-class Person(
-    val name: String,
-    val number: Long,
-    val company: String?
-) {}
--- src/main/kotlin/lesson_13/l13-t1.kt
+++ src/main/kotlin/lesson_13/l13-t1.kt
@@ -1,2 +1,7 @@
 package lesson_13
 
+class Person(
+    val name: String,
+    val number: Long,
+    val company: String?
+) {}
