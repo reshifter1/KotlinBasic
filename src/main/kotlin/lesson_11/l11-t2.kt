a1759cf2a0aec6a0c1c387719e55ee700e6cb4fe
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-12 17:36:53 +0300

    little refactor

--- src/main/kotlin/lesson_11/l11-t2.kt
+++ src/main/kotlin/lesson_11/l11-t2.kt
@@ -7,3 +7,31 @@ fun main() {
     user2.setPassword()
     user2.printParametres()
 }
+
+
+class User2(
+    val id: Int,
+    val username: String,
+    var password: String,
+    val email: String,
+    var bio: String = ""
+) {
+    fun printParametres() {
+        println("$id:$username:$password:$email")
+    }
+
+    fun setPassword() {
+        println("Введите текущий пароль:")
+        if (readln() == password) {
+            println("Введите новый пароль:")
+            password = readln()
+            println("Пароль обновлён")
+        } else println("Неверный пароль")
+    }
+
+    fun setBio() {
+        println("Введите биографию:")
+        bio = readln()
+        println("Биография обновлена")
+    }
+}
