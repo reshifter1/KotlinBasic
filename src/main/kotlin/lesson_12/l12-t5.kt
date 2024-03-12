6c77b5058d2f38399f552e2efde231bc2b4fd6e5
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-12 17:50:56 +0300

    little refactor

--- src/main/kotlin/lesson_12/l12-t5.kt
+++ src/main/kotlin/lesson_12/l12-t5.kt
@@ -1,5 +1,21 @@
 package lesson_12
 
+const val KELVIN_DIFFERENCE: Float = 273.15F
+
+class Weather5 (_dayWeather: Float, _nightWeather: Float, _hasFallout: Boolean) {
+    var dayWeather = _dayWeather - KELVIN_DIFFERENCE
+    var nightWeather = _nightWeather - KELVIN_DIFFERENCE
+    var hasFallout = _hasFallout
+
+    fun print() {
+        println("$dayWeather, $nightWeather, $hasFallout")
+    }
+
+    init {
+        print()
+    }
+}
+
 fun main() {
     val weatherList = mutableListOf<Weather5>()
 
