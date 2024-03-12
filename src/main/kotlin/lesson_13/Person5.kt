f54093d53e40b3e0606f4a4cd896576654255eb5
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-12 17:56:22 +0300

    little refactor

--- src/main/kotlin/lesson_13/Person5.kt
+++
@@ -1,9 +0,0 @@
-package lesson_13
-
-class Person5(
-    val name: String,
-    val number: Long,
-    val company: String? = null
-) {
-    fun print() { println("Имя: $name\nНомер: $number\nКомпания: ${company ?: "<не указано>"}") }
-}
--- src/main/kotlin/lesson_13/l13-t5.kt
+++ src/main/kotlin/lesson_13/l13-t5.kt
@@ -1,5 +1,13 @@
 package lesson_13
 
+class Person5(
+    val name: String,
+    val number: Long,
+    val company: String? = null
+) {
+    fun print() { println("Имя: $name\nНомер: $number\nКомпания: ${company ?: "<не указано>"}") }
+}
+
 fun main() {
     val contacts = mutableListOf<Person5>()
 
