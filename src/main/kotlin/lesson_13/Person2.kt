be40ec1d933fa822924d11ff32d151bd6fd2caff
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-12 17:53:29 +0300

    little refactor

--- src/main/kotlin/lesson_13/Person2.kt
+++ src/main/kotlin/lesson_13/Person2.kt
@@ -1,9 +1,2 @@
 package lesson_13
 
-class Person2(
-    val name: String,
-    val number: Long,
-    val company: String? = null
-) {
-    fun print() { println("Имя: $name\nНомер: $number\nКомпания: ${company ?: "<не указано>"}") }
-}
--- src/main/kotlin/lesson_13/l13-t2.kt
+++ src/main/kotlin/lesson_13/l13-t2.kt
@@ -1,5 +1,13 @@
 package lesson_13
 
+class Person2(
+    val name: String,
+    val number: Long,
+    val company: String? = null
+) {
+    fun print() { println("Имя: $name\nНомер: $number\nКомпания: ${company ?: "<не указано>"}") }
+}
+
 fun main() {
     val person2 = Person2("Ростислав", 89123456789, "Reddit")
 
