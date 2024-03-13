b9074b7b2031845a3f6faf9f8f01dba5000ae5b6
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-13 23:57:12 +0300

    KS-15-1 did

--- src/main/kotlin/lesson_15/l15-t1.kt
+++ src/main/kotlin/lesson_15/l15-t1.kt
@@ -1,2 +1,38 @@
-package lesson_10
+package lesson_15
 
+interface Flying {
+    fun fly() {
+        println("Я лечу!")
+    }
+}
+
+interface Walking {
+    fun walk() {
+        println("Я хожу!")
+    }
+}
+
+interface Swimming {
+    fun swim() {
+        println("Я плыву!")
+    }
+}
+
+class Carp() : Swimming
+
+class Seagull() : Flying, Walking
+
+class Duck() : Flying, Walking, Swimming
+
+fun main() {
+    val carp = Carp()
+    val seagull = Seagull()
+    val duck = Duck()
+
+    carp.swim()
+    seagull.fly()
+    seagull.walk()
+    duck.fly()
+    duck.walk()
+    duck.swim()
+}
