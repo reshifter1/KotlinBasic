f0e559d09c0e813387a4e2f0e5f3674d942557cd
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-14 18:28:53 +0300

    KS-16-1 fix

--- src/main/kotlin/lesson_16/l16-t1.kt
+++ src/main/kotlin/lesson_16/l16-t1.kt
@@ -1,2 +1,19 @@
-package lesson_10
+package lesson_16
 
+class Dice {
+    private val number: Int
+
+    init {
+        number = (1..6).random()
+    }
+
+    fun getNumber(): Int {
+        return number
+    }
+}
+
+fun main() {
+    val dice = Dice()
+    val rollResult = dice.getNumber()
+    println("Результат броска кубика: $rollResult")
+}
