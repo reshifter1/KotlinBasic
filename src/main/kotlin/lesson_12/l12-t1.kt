778ac4aa98a5cd67b26e1217de116e59050eb3a8
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-14 18:14:11 +0300

    KS-12-1 fix

--- src/main/kotlin/lesson_12/l12-t1.kt
+++ src/main/kotlin/lesson_12/l12-t1.kt
@@ -1,16 +1,25 @@
 package lesson_12
 
 fun main() {
-    val day1 = Weather(32, 12, true)
-    val day2 = Weather(31, 22, false)
+    val day1 = Weather().apply {
+        dayWeather = 32
+        nightWeather = 12
+        hasFallout = true
+    }
+    val day2 = Weather().apply {
+        dayWeather = 31
+        nightWeather = 22
+        hasFallout = false
+    }
     day2.hasFallout = true
+
     day2.printFallout()
 }
 
-class Weather(_dayWeather: Int, _nightWeather: Int, _hasFallout: Boolean) {
-    var dayWeather = _dayWeather
-    var nightWeather = _nightWeather
-    var hasFallout = _hasFallout
+class Weather {
+    var dayWeather: Int = 0
+    var nightWeather: Int = 0
+    var hasFallout: Boolean = false
 
     fun printFallout() {
         println("hasFallout: $hasFallout")
