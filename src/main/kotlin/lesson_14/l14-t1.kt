c08d252756083352034147af781f2169ee569e1d
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-16 21:02:32 +0300

    KS-14-1 fix

--- src/main/kotlin/lesson_14/l14-t1.kt
+++ src/main/kotlin/lesson_14/l14-t1.kt
@@ -1,13 +1,14 @@
 package lesson_14
 
 open class Liner(
-    val capacity: Int = 30,
-    val speed: Int = 50
-) {}
+    val capacityCargo: Int = 30,
+    val speed: Int = 50,
+    val capacityPeople: Int = 50
+)
 
-class HeavyShip() : Liner(50, 30) {}
+class HeavyShip() : Liner(50, 30, 40) {}
 
-class IceBreaker() : Liner(25, 25) {
+class IceBreaker() : Liner(25, 25, 30) {
     fun crushIce() {
         println("Вжжжжжжж")
     }
@@ -17,6 +18,8 @@ fun main() {
     val liner = Liner()
     val heavyShip = HeavyShip()
     val iceBreaker = IceBreaker()
+
+    iceBreaker.crushIce()
 }
 
 
