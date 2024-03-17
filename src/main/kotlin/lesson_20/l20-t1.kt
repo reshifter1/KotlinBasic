c29a124761e730572a715da09bc898af79f7c2a5
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-17 23:21:27 +0300

    KS-22-1 fix

--- src/main/kotlin/lesson_20/l20-t1.kt
+++ src/main/kotlin/lesson_20/l20-t1.kt
@@ -1,2 +1,9 @@
 package lesson_20
 
+val greeting: (String) -> String = { username -> "С наступающим Новым Годом, $username!" }
+
+fun main() {
+    val username = "John"
+    val greeting = greeting(username)
+    println(greeting)
+}
--- src/main/kotlin/lesson_22/l22-t1.kt
+++ src/main/kotlin/lesson_22/l22-t1.kt
@@ -1,9 +1,1 @@
 package lesson_22
-
-val greeting: (String) -> String = { username -> "С наступающим Новым Годом, $username!" }
-
-fun main() {
-    val username = "John"
-    val greeting = greeting(username)
-    println(greeting)
-}
