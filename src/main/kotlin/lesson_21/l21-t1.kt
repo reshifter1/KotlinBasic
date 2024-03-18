979d45984115f671b3624a986c254e108076bfe7
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-18 11:09:58 +0300

    KS-21-1 did

--- src/main/kotlin/lesson_21/l21-t1.kt
+++ src/main/kotlin/lesson_21/l21-t1.kt
@@ -1,2 +1,14 @@
 package lesson_21
 
+fun String.vowelCount(): Int {
+    val vowel = "уеыаоэяиюeyuioa"
+    var counter = 0
+    for (letter in this.lowercase()) {
+        if (letter in vowel) counter += 1
+    }
+    return counter
+}
+
+fun main() {
+    println("hello".vowelCount())
+}
