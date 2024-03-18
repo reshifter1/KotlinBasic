e7e56c2d1585d34fbeaf63974dbd62e0d5ea48db
pstryuk <pavel.stryuk.ps@gmail.com>
2024-03-18 13:25:50 +0300

    KS-17-1 fix

--- src/main/kotlin/lesson_17/l17-t1.kt
+++ src/main/kotlin/lesson_17/l17-t1.kt
@@ -1,16 +1,16 @@
 package lesson_17
 
-class Quiz {
-    val question1 = "Кто победит на выборах в 2024 году?"
+class Quiz (_question1: String, _answer1: String) {
+    val question1 = _question1
         get() = field
 
-    var answer1 = "Владимир Путин"
+    var answer1 = _answer1
         get() = field
         set(value) {field = value}
 }
 
 fun main() {
-    val quiz = Quiz()
+    val quiz = Quiz("Кто победит на выборах в 2024 году?", "Владимир Путин")
     println(quiz.question1)
     println(quiz.answer1)
 }
